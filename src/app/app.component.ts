9e4e0b9e3a4a81bd7b9898e0666eecae9e4c1411
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-07 00:20:15 +0300

    After lesson on 06.03.2021 (Front-End-Day-1).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+//Html'in datasının yönetildiği yer component'dir.
+//Java script'te ssl parantez obje demektir.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'Mustafa Sarıgül';
 }
